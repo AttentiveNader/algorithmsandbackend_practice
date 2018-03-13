ee9175b6c0bf1c29ceee3f90a5f560992bfb087d
NaderJS <legandryeagle@gmail.com>
2018-03-13 22:56:13 +0200

    fixing the gradient descent algorithm

--- algorithms/MachineLearning/gradient_descent/gradient_descent.go
+++ algorithms/MachineLearning/gradient_descent/gradient_descent.go
@@ -17,34 +17,47 @@ var (
 func StepGradient(m float64, b float64, points [][]float64, learningRate float64) (float64, float64) {
 	mG := float64(0)
 	bG := float64(0)
+	N := float64(len(points))
 	totalLoss = float64(0)
+	losses := float64(0)
 	fmt.Println("-----------NEW Context-----------")
 	for i := 0; i < len(points); i++ {
 		y := points[i][1]
 		x := points[i][0]
 		yG := m*x + b
 		err := y - yG
-		mG += (err * x)
-		bG += (err)
+		mG += (float64(2) / N) * err * x
+		bG += (float64(2) / N) * err
 		totalLoss += (err * err)
+		if err < 0 {
+			losses += (-err)
+		} else {
+			losses += err
+		}
+		//losses += err
 		//	fmt.Println(y, yG, "LOSS", err, "mG", (mG), "f'(m)", (err * x))
 	}
-	fmt.Println("TotalLoss ____________________ ", totalLoss)
-	fmt.Println("-------------weights------------", "m ", (m + mG*learningRate), "b ", (b + bG*learningRate))
-	return (m + (mG * learningRate)), (b + (bG * learningRate))
+	fmt.Println("TotalLoss ____________________ ", (totalLoss / N))
+	fmt.Println("-------------weights------------", "m ", (m - mG*learningRate), "b ", (b - bG*learningRate), "averageLoss", (losses / float64(100)))
+	return (m + mG*learningRate), (b + bG*learningRate)
 }
 
 func GradientDescent(recs [][]float64) {
-	learningRate := float64(0.000008)
+	learningRate := 0.0001
 	m := float64(2.0)
 	b := float64(5.0)
-	for i := 0; i < 100000; i++ {
+	for i := 0; i < 10; i++ {
+		/*	if i%10000 == 0 && i > 10000 {
+			if learningRate < 0.01 {
+				learningRate = learningRate * (float64(10))
+			}
+		}*/
 		m, b = StepGradient(m, b, recs, learningRate)
 		/*yG := m*recs[i][0] + b
 		totalLoss += (recs[i][1] - yG) * (recs[i][1] - yG)
 		fmt.Println(recs[i][1], yG, "LOSS", (recs[i][1]-yG)*(recs[i][1]-yG))*/
 	}
-	fmt.Println("learningRate", learningRate)
+	fmt.Println("learningRate", learningRate, "m", m, "b", b)
 }
 
 func main() {
